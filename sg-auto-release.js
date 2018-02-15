8d6b9753a7f73ce50f78cbf513667b91a2fa216b
Shilo Mangam <smangam@gmail.com>
2018-02-15 16:27:36 +0200

    added promise

--- sg-auto-release.js
+++ sg-auto-release.js
@@ -1,44 +1,52 @@
 const {spawn} = require('child_process');
 const https = require('https');
 
-function sgAutorelease({repo, sourceDirectory, pr, githubToken, rootPath}) {
+async function sgAutorelease({repo, sourceDirectory, pr, githubToken, rootPath}) {
   const [repoOwner, repoName] = repo.split('/');
   const deployDomain = `https://${repoOwner}-${repoName}-pr-${pr}.surge.sh`;
-  surgeDeploy({sourceDirectory, deployDomain, rootPath});
+  await surgeDeploy({sourceDirectory, deployDomain, rootPath});
   gitAddComment({repo, pr, deployDomain, githubToken});
 }
 
 function surgeDeploy({sourceDirectory, deployDomain, rootPath}) {
-  const deployPath = `${rootPath}/${sourceDirectory}`;
-  console.log(`Deploying to Surge from: ${deployPath}...`);
-  const surgeProcess = spawn('node', [`${process.cwd()}/node_modules/.bin/surge`, '--project', deployPath, '--domain', deployDomain]);
-  const msg = {
-    stdout: '',
-    stderr: ''
-  };
+  return new Promise((resolve, reject) => {
+    const deployPath = `${rootPath}/${sourceDirectory}`;
+    console.log(`Deploying to Surge from: ${deployPath}...`);
+    const surgeProcess = spawn('node', [`${process.cwd()}/node_modules/.bin/surge`, '--project', deployPath, '--domain', deployDomain]);
+    const msg = {
+      stdout: '',
+      stderr: ''
+    };
 
-  surgeProcess.stdout.on('data', data => {
-    msg.stdout += data.toString();
+    surgeProcess.stdout.on('data', data => {
+      msg.stdout += data.toString();
+    });
+
+    surgeProcess.stderr.on('data', data => {
+      msg.stderr += data.toString();
+    });
+
+    surgeProcess.on('error', e => {
+      console.log('Error on surge process', e);
+    });
+
+    surgeProcess.on('close', () => {
+      console.log('Surge process has finished.');
+      const stdout = msg.stdout.trim();
+      const stderr = msg.stderr.trim();
+      if (stdout) {
+        resolve();
+        console.log(`Surge process stdout: ${msg.stdout}`);
+      }
+      if (stderr) {
+        console.log(`Surge process stderr: ${msg.stderr}`);
+        reject();
+      }
+    });
   });
 
-  surgeProcess.stderr.on('data', data => {
-    msg.stderr += data.toString();
-  });
-
-  surgeProcess.on('error', e => {
-    console.log('Error on surge process', e);
-  });
-
-  surgeProcess.on('close', () => {
-    console.log('Surge process has finished.');
-    if (msg.stdout) {
-      console.log(`Surge process stdout: ${msg.stdout}`);
-    }
-    if (msg.stderr) {
-      console.log(`Surge process stderr: ${msg.stderr}`);
-    }
-  });
 }
+
 
 function gitAddComment({repo, pr, deployDomain, githubToken}) {
   const githubCommentsPath = `/repos/${repo}/issues/${pr}/comments`;
