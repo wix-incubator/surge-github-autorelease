4ec2c62973c863f34292a29b3bbc6d8bab5b888b
Ronen Stern <ronens@wix.com>
2018-05-10 23:38:31 +0300

    Lift scope

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,26 @@ const createGithubService = require('./createGithubService').default;
 const deploy = require('./deploy').default;
 const exec = require('child_process').exec;
 
+const fileService = {
+  exists: path => fs.existsSync(path)
+};
+const surgeService = command => new Promise((resolve, reject) => {
+  exec(`npx surge ${command.join(' ')}`, {timeout: 10000}, (error, stdout, stderr) => {
+    if (error && error.code !== 0) {
+      reject(new Error(`Surge error: ${error.code} ${stderr}`));
+    } else {
+      resolve(stdout);
+    }
+  });
+});
+
 async function sgAutorelease({rootPath, sourceDirectory, domain, repoOwner, repoName, githubToken, pr}) {
   const githubService = createGithubService(octokit, {
     owner: repoOwner,
     repo: repoName,
     token: githubToken
   };
-  const fileService = {
-    exists: path => fs.existsSync(path)
-  };
-  const surgeService = command => new Promise((resolve, reject) => {
-    exec(`npx surge ${command.join(' ')}`, {timeout: 10000}, (error, stdout, stderr) => {
-      if (error && error.code !== 0) {
-        reject(new Error(`'Surge error: ${error.code} ${stderr}`));
-      } else {
-        resolve(stdout);
-      }
-    });
-  });
+
   await deploy({rootPath, sourceDirectory, domain, pr, surgeService, fileService, githubService});
 }
 
